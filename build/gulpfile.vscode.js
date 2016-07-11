23ec09c555bebdbe3325a82610c3b8448e6c9bf9
Robert Konrad <git2016@robdangero.us>
2016-07-11 02:03:03 +0200

    Attempt to fix macOS version number.

--- build/gulpfile.vscode.js
+++ build/gulpfile.vscode.js
@@ -116,7 +116,7 @@ var config = {
 gulp.task('electron', function () {
 	// Force windows to use ia32
 	var arch = process.env.VSCODE_ELECTRON_PLATFORM || (process.platform === 'win32' ? 'ia32' : process.arch);
-	return electron.dest(path.join(build, 'electron'), _.extend({}, config, { arch: arch, ffmpegChromium: true }));
+	return electron.dest(path.join(build, 'electron'), _.extend({}, config, { arch: arch, ffmpegChromium: true, productVersion: packageJson.kodeStudioVersion }));
 });
 
 function mixinProduct() {
