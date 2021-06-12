4e8616e486ddb3c8f933af7657a836ab0b93a1fc
Robert Konrad <git2016@robdangero.us>
2021-06-12 23:25:06 +0200

    Improve code-hygiene

--- build/gulpfile.kode.js
+++ build/gulpfile.kode.js
@@ -1,3 +1,10 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Kode Studio team. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+'use strict';
+
 const path = require('path');
 const gulp = require('gulp');
 
