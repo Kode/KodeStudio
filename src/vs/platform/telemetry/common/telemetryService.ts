964fc33046997e81830f1cdc645f94c773161dca
Robert Konrad <git2016@robdangero.us>
2021-06-12 17:02:06 +0200

    Disable telemetry by default

--- src/vs/platform/telemetry/common/telemetryService.ts
+++ src/vs/platform/telemetry/common/telemetryService.ts
@@ -222,7 +222,7 @@ Registry.as<IConfigurationRegistry>(Extensions.Configuration).registerConfigurat
 				!product.privacyStatementUrl ?
 					localize('telemetry.enableTelemetry', "Enable usage data and errors to be sent to a Microsoft online service.") :
 					localize('telemetry.enableTelemetryMd', "Enable usage data and errors to be sent to a Microsoft online service. Read our privacy statement [here]({0}).", product.privacyStatementUrl),
-			'default': true,
+			'default': false,
 			'restricted': true,
 			'scope': ConfigurationScope.APPLICATION,
 			'tags': ['usesOnlineServices']
