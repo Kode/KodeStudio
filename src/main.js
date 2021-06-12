180e7f9eec32be58d9d096a761a215678daffa0f
Robert Konrad <git2016@robdangero.us>
2021-06-12 17:11:23 +0200

    Allow Electron to start other apps

--- src/main.js
+++ src/main.js
@@ -40,6 +40,59 @@ const args = parseCLIArgs();
 const userDataPath = getUserDataPath(args);
 app.setPath('userData', userDataPath);
 
+let nlsConfigurationPromise = undefined;
+let metaDataFile = undefined;
+
+function findKhaAppParameter() {
+	var argv = process.argv.slice(1);
+	if (argv[0] !== '--chromedebug') return null;
+	for (var i = 1; i < argv.length; i++) {
+		if (argv[i].trim().length > 0 && argv[i] !== '.' && argv[i][0] !== '-') {
+			return argv[i];
+		}
+	}
+	return null;
+}
+
+// start in Kha debug mode
+if (findKhaAppParameter()) {
+	try {
+		// Override app name and version.
+		var file = findKhaAppParameter();
+		var packagePath = path.resolve(file);
+		var packageJsonPath = path.join(packagePath, 'package.json');
+		if (fs.existsSync(packageJsonPath)) {
+			var packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
+			if (packageJson.version)
+				app.setVersion(packageJson.version);
+			if (packageJson.productName)
+				app.setName(packageJson.productName);
+			else if (packageJson.name)
+				app.setName(packageJson.name);
+			app.setPath('userData', path.join(app.getPath('appData'), app.getName()));
+			app.setPath('userCache', path.join(app.getPath('cache'), app.getName()));
+			app.setAppPath(packagePath);
+		}
+
+		// Run the app.
+		require('module')._load(packagePath, module, true);
+	} catch(e) {
+		if (e.code == 'MODULE_NOT_FOUND') {
+			app.focus();
+			dialog.showErrorBox(
+				'Error opening app',
+				'The app provided is not a valid Electron app, please read the docs on how to write one:\n' +
+				`https://github.com/atom/electron/tree/v${process.versions.electron}/docs\n\n${e.toString()}`
+			);
+			process.exit(1);
+		} else {
+			console.error('App threw an error when running', e);
+			throw e;
+		}
+	}
+}
+else {
+
 // Configure static command line arguments
 const argvConfig = configureCommandlineSwitchesSync(args);
 
@@ -80,9 +133,8 @@ const nodeCachedDataDir = getNodeCachedDir();
  *
  * @type {Promise<NLSConfiguration> | undefined}
  */
-let nlsConfigurationPromise = undefined;
 
-const metaDataFile = path.join(__dirname, 'nls.metadata.json');
+metaDataFile = path.join(__dirname, 'nls.metadata.json');
 const locale = getUserDefinedLocale(argvConfig);
 if (locale) {
 	const { getNLSConfiguration } = require('./vs/base/node/languagePacks');
@@ -134,6 +186,8 @@ async function onReady() {
 	} catch (error) {
 		console.error(error);
 	}
+}
+
 }
 
 /**
